0e39a30ce7770709816b9f277545deed6bdebe30
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2025-07-16 15:41:42 +1000

    Update boot.sh

--- build/boot.sh
+++ build/boot.sh
@@ -2,18 +2,17 @@
 set -e
 
 # Copy missing default files (but never overwrite)
-for f in app.py logstash.conf start-logstash.sh; do
+for f in app.py logstash.conf; do
   if [ ! -f /opt/syslog-multiplier/$f ]; then
     cp /usr/local/bin/$f /opt/syslog-multiplier/$f
     echo "Copied missing $f to /opt/syslog-multiplier/"
   fi
 done
 
-# Ensure start script is executable
-chmod +x /opt/syslog-multiplier/start-logstash.sh
+
 
 # Start Logstash in the background
-bash /opt/syslog-multiplier/start-logstash.sh &
+bash /usr/local/bin/start-logstash.sh &
 
 # Start tailing log file (background)
 tail -F /opt/syslog-multiplier/logstash.log &
